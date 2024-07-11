b349d740e22781534c983faa419114b5eb8a8d5c
Lewbie <lewis6870+github@gmail.com>
2024-07-11 12:46:17 +0100

    removing useless comments

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ def main():
         l_dict = letter_count(open_book)
         report(l_dict, word_total)
 
-    #returns the total word count
 def word_count(book):
     words_total = len(book.split())
     return words_total
 
-    #returns the number of times each character appears
 def letter_count(book):
     total = book.lower()
     dict = {}
@@ -22,16 +20,13 @@ def letter_count(book):
     return dict
 
 def report(dict,count):
-    #converting dictionary of letter:count pairs into a list of dictionaries
     letter_list = []
     for key, value in dict.items():
         pair = {"letter":key,"count":value}
         letter_list.append(pair)
     
-    #sorting the order
     letter_list.sort(reverse=True,key=sort_on)
 
-    #printing out the report
     print("  --- Begin report of books/frankenstein.txt ---")
     print(f"\t{count} words found in the document\n")
     for i in range(len(letter_list)):
@@ -39,7 +34,6 @@ def report(dict,count):
             print(f"\tThe '{letter_list[i]['letter']}' character was found {letter_list[i]['count']} times")      
     print("\t\t--- End report ---")
 
-    #function for sorting the order
 def sort_on(dict):
     return dict["count"]
 
